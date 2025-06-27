1ac1e3cf4fdbdd94f6dd815e2f189205e429782a
Brian Gardner <brian@briangardner.com>
2025-06-26 21:20:48 -0700

    Better a11y for sidebar heading.

--- patterns/sidebar.php
+++ patterns/sidebar.php
@@ -8,8 +8,8 @@
 ?>
 <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20","padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60","right":"var:preset|spacing|60"}}},"backgroundColor":"neutral","fontSize":"x-small","layout":{"type":"default"}} -->
 <div class="wp-block-group has-neutral-background-color has-background has-x-small-font-size" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)">
-	<!-- wp:heading {"level":3} -->
-	<h3 class="wp-block-heading"><?php echo esc_html__( 'About the author', 'indio' ); ?></h3>
+	<!-- wp:heading {"fontSize":"medium"} -->
+	<h2 class="wp-block-heading has-medium-font-size"><?php echo esc_html__( 'About the author', 'indio' ); ?></h2>
 	<!-- /wp:heading -->
 	<!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
 	<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ) . '/assets/images/brian-gardner.jpg'; ?>" alt="Brian Gardner"/></figure>
